9b9c6d3b6af3f847132ec4fdce7d8de7c5513ea9
Kevin Herreros <kev.herreros@gmail.com>
2022-09-26 10:55:28 -0500

    feat :uso de script externo

---
+++ script.js
@@ -0,0 +1,7 @@
+
+      console.log("saoko papi saoko");
+      document.getElementById("generation").style.color ="grey";
+      function colorTitulo() {
+        console.log("Pulso el boton de Generation");
+        document.getElementById("generation").style.color ="blue";
+      }
